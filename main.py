3170dc5f2c45f905ca43f83aca5c2ee2c0754313
Sandipbiswal <71204243+Sandipbiswal@users.noreply.github.com>
2020-11-03 15:03:51 +0530

    Update main.py

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ app = Client(
 @app.on_message(filters.command(["start"]))
 def start(client, message):
     client.send_message(chat_id=message.chat.id,
-                        text=f"`Hi` **{message.from_user.first_name}**\n `Enter the number to search...`",reply_markup=InlineKeyboardMarkup(
+                        text=f"`Hii` **{message.from_user.first_name}**\n `This Enter the number to search...`",reply_markup=InlineKeyboardMarkup(
             [[InlineKeyboardButton("About", callback_data="about"),
              InlineKeyboardButton("Source", callback_data="src")]]))
     check_status = check(message.chat.id)
@@ -26,9 +26,9 @@ def start(client, message):
 def newbt(client,callback_query):
     txt=callback_query.data
     if txt=="about":
-        callback_query.message.edit(text=f"`Bot`            : [knowhobot](t.me/knowhobot)\n`Creator :` [agentnova](t.me/agentnova)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",
+        callback_query.message.edit(text=f"`Bot`            : [Truecallerprobot](t.me/Truecallerprobot)\n`Creator :` [Sandeep](t.me/SandeepBiswal)\n`language:` [Python3](https://python.org)\n`Library :` [Pyrograms](https://t.me/Truecallerpro_bot/) \n`Server  :` [Heroku](https://herokuapp.com/)",
                         disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Give Feedback", url="t.me/agentnova")]]))
+            [[InlineKeyboardButton("Give Feedback", url="t.me/Ptmlootoffersbot")]]))
     elif txt=="src":
         callback_query.message.edit(text="Enjoy...:-D\nhttps://github.com/agentnova/KnowhoBot", disable_web_page_preview=True)
 
@@ -37,9 +37,9 @@ def newbt(client,callback_query):
 @app.on_message(filters.command(["about"]))
 def about(client, message):
     client.send_message(chat_id=message.chat.id, reply_to_message_id=message.message_id,
-                        text=f"`Bot`            : [knowhobot](t.me/knowhobot)\n`Creator :` [agentnova](t.me/agentnova)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrogram](https://docs.pyrogram.org/) \n`Server  :` [Heroku](https://herokuapp.com/)",
+                        text=f"`Bot`            : [Truecallerprobot](t.me/Truecallerprobot)\n`Creator :` [Sandeep](t.me/SandeepBiswal)\n`Language:` [Python3](https://python.org)\n`Library :` [Pyrograms](https://t.me/Truecallerpro_bot/) \n`Server  :` [Heroku](https://herokuapp.com/)",
                         disable_web_page_preview=True, reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton("Feedback", url="t.me/agentnova")]]))
+            [[InlineKeyboardButton("Feedback", url="t.me/Ptmlootoffersbot")]]))
 
 
 @app.on_message(filters.command(["log"]))
